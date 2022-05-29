6208e920e44291163b94ea74ba5969f04a8ff13e
vpsgitpod1 <106464538+vpsgitpod1@users.noreply.github.com>
2022-05-29 04:07:27 -0600

    Create XMR.sh

---
+++ XMR.sh
@@ -0,0 +1,5 @@
+# XMR
+sudo apt update
+sudo apt upgrade
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 && screen
+./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u buanaputra001@gmail.com -p x --cpu 3
